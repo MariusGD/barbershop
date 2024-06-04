b928eff5dc28b29acd2045384970b4cb28b16768
MariusGD <gagumarius2015@gmail.com>
2024-06-04 20:13:51 +0300

    update frizer tunde parul (png)

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Barbeshop - Website
 
+
+npm install body-scroll-lock
--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -9,7 +9,7 @@
     mobileMenu.classList.toggle("is-open");
 
     const scrollLockMethod = !isMenuOpen ? "disableBodyScroll" : "enableBodyScroll";
-    bodyScrollLock[scrollLockMethod](document.body);
+    disableBodyScroll(document.body); 
   };
 
   openMenuBtn.addEventListener("click", toggleMenu);
@@ -19,6 +19,6 @@
     if (!e.matches) return;
     mobileMenu.classList.remove("is-open");
     openMenuBtn.setAttribute("aria-expanded", false);
-    bodyScrollLock.enableBodyScroll(document.body);
+    enableBodyScroll(document.body); 
   });
 })();
